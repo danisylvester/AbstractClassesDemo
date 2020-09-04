1a39fd89a886766e7a40802ff7ac72809d87083e
danisylvester <maria.sylvester10@gmail.com>
2020-09-04 15:44:37 -0500

    Created Vehicle template class, and derived the two classes Motorcycles & Cars from Vehicle.

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Car : Vehicle
+    {
+        public bool HasTrunk { get; set; } = true;
+        
+        public override void DriveAbstranct()
+        {
+            Console.WriteLine("This car is in drive.");
+        }
+
+
+    }
+}
---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Motorcycle : Vehicle
+    {
+        public bool HasSideCart { get; set; } = false;
+        public override void DriveAbstranct()
+        {
+            Console.WriteLine("This motorcycle is in drive.");
+        }
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is a {GetType().BaseType.Name} that is virtually in drive.");
+        }
+    }
+}
