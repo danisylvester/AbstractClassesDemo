d82e193d0bb612659f35922e5767852e9622504a
danisylvester <maria.sylvester10@gmail.com>
2020-09-07 22:20:21 -0500

    created 4 objects of vehicles class, added objects to a list, created a foreach loop that prints the properties of each object.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,19 +19,23 @@ namespace ConsoleUI
             // Create a list of Vehicle called vehicles
             var vehicles = new List<Vehicle>();
 
+            Car subaru = new Car() { HasTrunk = true, Year = 2020, Make = "Subaru", Model = "Outback" };
+            Motorcycle harleyDavidson = new Motorcycle() {HasSideCart = true, Year = 2005, Make = "Harley Davidson", Model = "Chopper" };
+            Vehicle sedan = new Car() { Make = "Toyota", Model = "Corolla", Year = 2005 };
+            Vehicle sport = new Car() { HasTrunk = false, Make = "Dodge", Model = "Challenger", Year = 2019 };
 
-            /*
-             * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
-             * - new it up as one of each derived class
-             * Set the properties with object initializer syntax
-             */
+            // Adding the 4 vehicles to the list.
+            vehicles.Add(subaru);
+            vehicles.Add(harleyDavidson); 
+            vehicles.Add(sedan);
+            vehicles.Add(sport);
 
-            /*
-             * Add the 4 vehicles to the list
-             * Using a foreach loop iterate over each of the properties
-             */
-
-            // Call each of the drive methods for one car and one motorcycle
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($"Make: {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveAbstranct();
+                Console.WriteLine(".......................................................");
+            }
 
             #endregion            
             Console.ReadLine();
